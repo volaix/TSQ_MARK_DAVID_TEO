8258303348f01db574110d929ef9d443df9f65d3
Mark David Teo <volaix@gmail.com>
2024-07-26 11:21:03 +0800

    draggable posts work

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 import Image from "next/image"
+import dynamic from 'next/dynamic'
+const DynamicPostList = dynamic(() => import('./PostList'), { ssr: false })
 
 import { useQuery, gql, useMutation } from "@apollo/client"
+import PostList from "./PostList"
 
 export default function Home() {
-  const { loading, error, data, refetch } = useQuery(gql`
+  const { loading, error, data: postListResponse, refetch } = useQuery(gql`
   query getUsers {
     users {
       id
@@ -14,31 +17,24 @@ export default function Home() {
   }
 `)
 
+  console.log('postListResponse: ', postListResponse)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
       {error && <p>Error : {error.message}</p>}
       {loading && <p>Loading Posts...</p>}
 
-      <div className="h-4/5 w-1/3 px-4 py-6 flex flex-col gap-2 overflow-y-auto max-h-[700px] border border-orange-400 rounded">
-        <h1>list of all posts</h1>
-        {data?.users?.map((user: Record<string, string | null | number>) => {
-          return (
-            <div key={user?.id}>{user.title}</div>
-          )
-        }
-          // <Fragment key={user?.id}>
-          //   <UserBadge user={user} refetch={refetch} />
-          // </Fragment>
-        )}
-      </div>
-      {/* <div className="h-4/5 w-1/3 px-2 py-5 justify-center flex border border-orange-400 rounded"> */}
-      {/* <UserForm refetch={refetch} /> */}
-      {/* </div> */}
-
-
-
-
+      {/* <div className="h-4/5 w-1/3 px-4 py-6 flex flex-col gap-2 overflow-y-auto max-h-[700px] border border-orange-400 rounded"> */}
+      <h1>list of all posts</h1>
+      <section>
+        <DynamicPostList data={postListResponse} />
+      </section>
+      {/* {postListResponse?.users?.map(({ id, title }: Record<string, string | null | number>) => {
+        return (
+          <div key={id}>{title}</div>
+        )
+      }
+      )}  */}
     </main>
   )
 }
