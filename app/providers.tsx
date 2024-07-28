558965517726e21dfcb22baeb8122f6049dcf942
Mark David Teo <volaix@gmail.com>
2024-07-28 14:20:46 +0800

    moved files around

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,7 @@
 
 'use client'
 
-import client from "./apolloClient"
+import client from "./_apollo/apolloClient"
 import { ApolloProvider } from "@apollo/client"
 
 export default function Providers({
